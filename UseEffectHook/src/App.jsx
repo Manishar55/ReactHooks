443794226caa96910243592c52f5443ed51dbde2
Manisha Rana <manisharana02005@gmail.com>
2025-01-16 00:36:07 +0530

    Added UseEffect hook concept and cleanup concept

---
+++ UseEffectHook/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import CleanUp from './Components/CleanUp'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  // useEffect(()=>{
+  //   document.title=`${count} new messages !!`;   //we are using useEffect without any dependency, so it will run on every single change
+  // });
+
+  // useEffect(()=>{
+  //   document.title=`${count} new messages !!`;   //useEffect will run only one time, when our component gets render first time
+  // }, []); 
+
+  const [otherCount, otherSetCount] = useState(0);
+
+  useEffect(()=>{
+    document.title=`${otherCount} new messages !!`;   //useEffect will run only when state variable(otherCount) value will change
+  }, [otherCount]);  
+
+  return (
+    <>
+      <div>
+        <h1>{count} New Messages !!</h1>
+        <button onClick={()=>setCount(count+1)}> Increases</button>
+
+
+        <h1>Other Count: {otherCount}</h1>
+        <button onClick={()=>otherSetCount(otherCount+5)}> Increase by 5 </button>
+
+        <CleanUp/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ UseEffectHook/src/Components/CleanUp.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+function CleanUp() {
+
+    const [time, setTime] = useState(0);
+
+    //the interval will not stop unless we use the Clear Interval function, 
+    // if we are setting state using setInterval and that side effect is not cleaned up 
+    // when our components unmounts or we are no longer to use it the state is destroyed with the component 
+    // but the setInterval function will keep running that will make application slow
+    useEffect(()=>{
+        const timer= setInterval(()=>{
+            setTime(time+1);   
+        }, 1000);
+
+        return () =>{   //to use cleanup function we need to run return function in useEffect
+            clearInterval(timer);  //the cleanup function will be called when the component is unmounted 
+        }
+    });
+    
+
+    return(
+        <div>
+            <h1>{time} in seconds</h1>
+        </div>
+    )
+  
+}
+
+export default CleanUp
