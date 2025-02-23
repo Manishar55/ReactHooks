feccc5486a2d416ae13ad00226f6b44d90affa14
Manisha Rana <manisharana02005@gmail.com>
2025-02-23 16:40:40 +0530

    Exmaple for useReducer hook

--- UseReducerHook/src/App.jsx
+++ UseReducerHook/src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useReducer } from 'react'
 import './App.css'
 
+
+const ACTION={
+  INCREASE: "increase",
+  DECREASE: "decrease"
+}
+
+const initialState={ count : 0 };
+
+const reducer=(state, action)=>{
+  console.log(action);
+
+  switch(action.type){
+
+    case "increase":    //case ACTION.INCREASE:
+    return {count: state.count + 1};
+
+    case "decrease":
+    return {count: state.count - 1};
+
+    default:
+    return state;
+  }
+};
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  //reducer function will manage all state
+  const[state, dispatch] = useReducer(reducer, initialState);
+
+  const increaseCount=()=>{
+    dispatch({type: "increase"}); //whatever we pass in this dispatch function we can access this by the action parameter
+  }
+
+  const decreaseCount=()=>{
+    dispatch({type: "decrease"});  //or {type: "ACTION.DECREASE"}
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Count : {state.count}</h2>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
   )
 }
 
