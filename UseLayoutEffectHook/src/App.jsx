6e2679ff8b9c17705dbbb80409d4291f441358fc
Manisha Rana <manisharana02005@gmail.com>
2025-02-24 12:30:08 +0530

    code for useLayoutEffect Hook

--- UseLayoutEffectHook/src/App.jsx
+++ UseLayoutEffectHook/src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
 
-import './App.css'
+import { useState, useEffect, useLayoutEffect } from 'react'
+import { useRef } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [toggle, setToggle] = useState(false);
+  const textRef= useRef()
 
-  return (
+  // useEffect(()=>{
+  //   console.log("useEffect");
+
+  //   if(textRef.current!=null){
+
+  //     const dimenion= textRef.current.getBoundingClientRect();  //using this function we can get all dimensions of this element
+  //     console.log(dimenion);
+  //     textRef.current.style.paddingTop=`${dimenion.height}px`
+  //   }
+  // },[toggle]);
+
+  //useLayoutEffect runs before the DOM printed so it will not show the friction 
+  useLayoutEffect(()=>{  //useLayout will always run first  
+    console.log("useLayoutEffect");
+
+    if(textRef.current!=null){
+
+      const dimenion= textRef.current.getBoundingClientRect();  //using this function we can get all dimensions of this element
+      console.log(dimenion);
+      textRef.current.style.paddingTop=`${dimenion.height}px`
+    }
+  },[toggle]);
+
+  return(
     <>
-      <div>
-        <h1>UseLayoutEffect Hook</h1>
-      </div>
+      <button onClick={()=>setToggle(!toggle)}>Toggle</button>
+      {toggle && <h3 ref={textRef}>Code toggled</h3>}
     </>
   )
 }
